597d514d9b2b356887ba558ae7ef466a880e4959
Orest Chumak <orest.chumak@gmail.com>
2026-02-20 20:02:54 +0200

    Solved a problem with "func_is_never_used"

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 mod hackerrank;
 
+// have used to keep the func's names without a prefix "_".
+fn solve_func_is_never_used() {
+    if false {
+        hackerrank::task0::simple_array_sum(&[]);
+    }
+}
+
 fn main() {
     println!("We learn Rust!");
+    
+    solve_func_is_never_used(); // solve the same named warning
 }
